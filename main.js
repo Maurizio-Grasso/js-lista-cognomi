4356f66a3b4660628b009a3860205a71350283cd
maurizio <maurizio.grasso@gmail.com>
2021-02-24 17:56:52 +0100

    redOl

--- main.js
+++ main.js
@@ -18,7 +18,15 @@ listaCognomiNuova.sort();
 
 for ( i = 0; i < listaCognomiNuova.length; i++ ) {
 
+    // nell'HTML, all'interno della <ol> aggiunge un <li> contenente il nome dell'elemento iterato, al quale assegna un id univoco.
+    document.getElementById("my-list").innerHTML += "<li id=\"list-item-"+(i+1)+"\">"+listaCognomiNuova[i]+"</li>";
+    
     if ( cognomeUtente == listaCognomiNuova[i] ) {
-        console.log(cognomeUtente+" trovato in posizione "+(i+1));
+
+        console.log(cognomeUtente+" trovato in posizione "+(i+1));  // Indica la posizione umana che coincide con il cognome inserito
+        document.getElementById("list-item-"+(i+1)).style = "color:red;"    // e colora il rispettivo <li> di rosso nell'HTML
+    
     }
+
+
 }
